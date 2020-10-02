20e9146bab35e10fcfbed885e75bdb2e9d4bf30a
sindhushines <sindhushines@gmail.com>
2020-10-02 19:03:51 +0530

    message

--- src/com/browser/AxisBank.java
+++ src/com/browser/AxisBank.java
@@ -1,8 +1,10 @@
 package com.browser;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AxisBank {
@@ -13,6 +15,8 @@ public class AxisBank {
 		driver.get("https://www.facebook.com/");
 		Dimension d=new Dimension(800,400);
 		driver.manage().window().setSize(d);
+		WebElement txtPassword = driver.findElement(By.id("pass"));
+		txtPassword.sendKeys("Sindhuja");
 	}
 	
 
